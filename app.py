4f2fdad5b8138a2d8f7f81f1137c0a13679d2ea0
juank2808 <webjcaf022014@gmail.com>
2023-02-23 20:04:59 +0100

    Añadiendo rutas

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from fastapi import FastAPI
-from v1.scripts.create_tables import create_tables
+# from v1.scripts.create_tables import create_tables
 
-create_tables()
+from v1.routes.user_router import router as user_router
+
 app = FastAPI()
 
-
+app.include_router(user_router)
 @app.get('/')
 def home():
     return {"message": "Tonto el que lo lea"}
--- v1/routes/user_router.py
+++ v1/routes/user_router.py
@@ -2,3 +2,22 @@ from fastapi import APIRouter
 from fastapi import Depends
 from fastapi import status
 from fastapi import Body
+
+from ..schema import user_schema
+from ..service import user_service
+
+from ..utils.db import get_db
+
+router = APIRouter(prefix="/api/v1")
+
+@router.post (
+    "/user/",
+    tags=["users"],
+    status_code=status.HTTP_201_CREATED,
+    response_model=user_schema.User,
+    dependencies=[Depends(get_db)],
+    summary="Create a new user"
+)
+
+def create_user(user: user_schema.UserRegister = Body(...)):
+    return user_service.create_user(user)
