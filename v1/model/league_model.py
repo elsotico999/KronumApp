557aeeb8c6dd5081c36b6c1bdd2cfe0fa4d4652e
juank2808 <webjcaf022014@gmail.com>
2023-03-07 19:37:21 +0100

    Ahora con ligas

--- v1/model/league_model.py
+++ v1/model/league_model.py
@@ -5,6 +5,7 @@ from ..utils.db import db
 class League(peewee.Model):
     id = peewee.AutoField(unique=True, index=True)
     country_id = peewee.IntegerField()
+    name = peewee.CharField()
     trial534 = peewee.CharField()
 
     class Meta:
---
+++ v1/schema/league_schema.py
@@ -0,0 +1,14 @@
+from pydantic import BaseModel ,Field 
+from typing import Optional
+
+class LeagueBase(BaseModel):
+    name: Optional[str] = Field (
+        ...,
+        min_length=3,
+        max_length=50,
+        example="Liga de Fútbol"
+    )
+class League(LeagueBase):
+    id: Optional[int] = Field(..., example="1")
+
+    
