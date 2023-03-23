6cca9e7f98dd5651c437ba548ed28c222917ac8d
juank2808 <webjcaf022014@gmail.com>
2023-03-23 19:33:28 +0100

    LOGIN HECHO!

--- v1/routes/user_router.py
+++ v1/routes/user_router.py
@@ -8,23 +8,22 @@ from ..schema import user_schema
 from ..service import user_service
 from ..service import auth_service
 from ..schema.token_schema import Token
-
+from fastapi import Request
 from ..utils.db import get_db
 
 router = APIRouter(prefix="/api/v1")
 
-@router.post (
-    "/user/",
-    tags=["users"],
-    status_code=status.HTTP_201_CREATED,
-    response_model=user_schema.User,
+@router.post ("/user/",tags=["users"],status_code=status.HTTP_201_CREATED,response_model=user_schema.User,
     dependencies=[Depends(get_db)],
     summary="Create a new user"
 )
 def create_user(user: user_schema.UserRegister = Body(...)):
     return user_service.create_user(user)
-'''--'''
-@router.post ("/login",tags=["users"], response_model=Token)
-async def login_from_access_token(form_data:OAuth2PasswordRequestForm=Depends()):
-    access_token = auth_service.generate_token(form_data.username, form_data.password)
+'''--------------------------------------------------------'''
+
+@router.post ("/login", tags=['user'])
+async def login_from_access_token(request: Request):
+    data = dict(await request.json())
+    access_token = auth_service.generate_token(data['username'], data['password'])
     return Token(access_token=access_token, token_type="bearer")
+    
